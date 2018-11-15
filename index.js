004ac599e5267b100f46dbd52f689ee2fd782e04
samuelomotayo <samuelomotay@gmail.com>
2018-11-15 19:20:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ document.addEventListener("DOMContentLoaded", function(){
 })
 
 function fetchCurrentWeather(city) {
-  //fetch current weather based on city
+  fetch('http://example.com/movies.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+  });
 }
 
 function displayCurrentWeather(json) {
